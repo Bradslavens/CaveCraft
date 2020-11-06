3fcf5067d76102441afb40a513b487267354a200
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-11-06 09:35:21 -0800

    fresh scene

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -6,11 +6,13 @@ public class CubeGenerator : MonoBehaviour
 {
 
     [SerializeField] private GameObject _cube;
-    
+    private Vector3 _cubePosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cubePosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        object p = Instantiate(_cube, _cubePosition, Quaternion.identity);
     }
 
     // Update is called once per frame
