62b231f307f3b6c536cd6f0a55c23b51956b17db
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-11-06 11:24:03 -0800

    genterating prefab cube

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class CubeGenerator : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] private float _perlinNoise = 0f;
     [SerializeField] private float _refinement = 0f;
     [SerializeField] private float _multiplier = 0f;
     [SerializeField] private int _cubes = 0;
+
+    [Header("Other")]
+    [SerializeField] private GameObject _cube;
+
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < _cubes; i++){
             for(int j = 0; j < _cubes; j++){
                 _perlinNoise = Mathf.PerlinNoise(i * _refinement, j * _refinement);
-                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                go.transform.position = new Vector3(i, _perlinNoise * _multiplier, j);
+                Instantiate(_cube, new Vector3(i, _perlinNoise * _multiplier, j), Quaternion.identity);
             }
         }
         
