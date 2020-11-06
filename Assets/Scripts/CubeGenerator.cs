9a08cebbffaf071d42abe5613d8d60363401bffe
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-11-06 10:59:44 -0800

    updated cube generator script add Perlin Noise

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class CubeGenerator : MonoBehaviour
 {
-
-    [SerializeField] private GameObject _cube;
-    private Vector3 _cubePosition;
-
+    [SerializeField] private float _perlinNoise = 0f;
+    [SerializeField] private float _refinement = 0f;
+    [SerializeField] private float _multiplier = 0f;
+    [SerializeField] private int _cubes = 0;
     // Start is called before the first frame update
     void Start()
     {
-        for(int x = -10; x <= 10; x++){   
-            for(int z = -10; z <= 10; z++){
-                float y = Random.Range(0f, 3f);
-                Instantiate(_cube, new Vector3(x,y,z), Quaternion.identity);
-            }    
+        for(int i = 0; i < _cubes; i++){
+            for(int j = 0; j < _cubes; j++){
+                _perlinNoise = Mathf.PerlinNoise(i * _refinement, j * _refinement);
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.position = new Vector3(i, _perlinNoise * _multiplier, j);
+            }
         }
+        
     }
 
     // Update is called once per frame
